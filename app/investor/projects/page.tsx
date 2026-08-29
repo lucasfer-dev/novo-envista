4055daae75b8662d251b9342667684c983e853a0
Lucas Fernandes | Full Stack Developer <akaakashiseijuro@gmail.com>
2026-08-28 22:04:18 -0300

    feat: redirect investor owned projects to discovery

---
+++ app/investor/projects/page.tsx
@@ -0,0 +1,7 @@
+import { redirect } from "next/navigation";
+import { requireProductUser } from "@/lib/auth/require-product-user";
+
+export default async function InvestorProjectsIndexPage() {
+  await requireProductUser("investor");
+  redirect("/investor/explore");
+}
